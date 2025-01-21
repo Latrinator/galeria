4acd6ad9f01ec8939af6df22b7a616e552cb3b82
Latrinator <ppoduszkajas53@gmail.com>
2025-01-21 20:08:12 +0100

    logowanie i poprawki w bazach danych

--- polls/admin.py
+++ polls/admin.py
@@ -3,8 +3,18 @@
 from django.contrib import admin
 from .models import Exhibit
 from .models import Artist
+from .models import Storage
+from .models import Gallery
+from .models import Renter
+from .models import Rented
+from .models import Exhibition
 
 
 
 admin.site.register(Exhibit)
 admin.site.register(Artist)
+admin.site.register(Storage)
+admin.site.register(Gallery)
+admin.site.register(Renter)
+admin.site.register(Exhibition)
+admin.site.register(Rented)
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
-def index(request):
-    return HttpResponse("Hello im a bean.")
+
+def logowanie(request):
+    template = loader.get_template('logowanie.html')
+    return HttpResponse(template.render())
