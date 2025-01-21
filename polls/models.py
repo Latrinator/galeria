9570eb3786f3a8b0a72d8bbae543377a5367fc20
Latrinator <ppoduszkajas53@gmail.com>
2025-01-21 19:32:33 +0100

    finalizacja bazy danych

--- polls/models.py
+++ polls/models.py
@@ -19,3 +19,34 @@ class Artist(models.Model):
     surname = models.CharField(max_length=30)
     date_of_birth = models.DateField()
     date_of_death = models.DateField(null=True, blank=True)
+
+class Storage(models.Model):
+    id_storage_session = models.AutoField(primary_key=True)
+    id_exhibit = models.ForeignKey('Exhibit', on_delete=models.CASCADE)
+    cause = models.CharField(max_length=40)
+    since = models.DateField()
+    until = models.DateField(null=True, blank=True)
+
+class Gallery(models.Model):
+     id_gallery = models.AutoField(primary_key=True)
+     name = models.CharField(max_length=30)
+     localisation = models.CharField(max_length=30)
+
+class Renter(models.Model):
+     id_renter = models.AutoField(primary_key=True)
+     name = models.CharField(max_length=30)
+     localisation = models.CharField(max_length=30)
+
+class Exhibition(models.Model):
+    id_exhibition = models.AutoField(primary_key=True)
+    id_exhibit = models.ForeignKey('Exhibit', on_delete=models.CASCADE)
+    id_gallery = models.ForeignKey('Gallery', on_delete=models.CASCADE)
+    since = models.DateField()
+    until = models.DateField(null=True, blank=True)
+
+class Rented(models.Model):
+    id_rented = models.AutoField(primary_key=True)
+    id_exhibit = models.ForeignKey('Exhibit', on_delete=models.CASCADE)
+    id_renter = models.ForeignKey('Gallery', on_delete=models.CASCADE)
+    since = models.DateField()
+    until = models.DateField(null=True, blank=True)
