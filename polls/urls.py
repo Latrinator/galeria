ef6a47c744d964a03fa41f5367c728f585aa80cd
Latrinator <ppoduszkajas53@gmail.com>
2025-01-29 22:22:36 +0100

    wielkie zmiany guest zrobiony

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.logowanie, name="logowanie"),
+    path("guest/", views.guest, name='guest'),
+    path("bananas/", views.bananas, name='bananas'),
+    path("api/artists/", views.api_artists, name='api_artists'),
+    path("api/exhibits/", views.api_exhibits, name='api_exhibits'),
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.http import JsonResponse
+from .models import Artist,Exhibit
+
 
 
 
 def logowanie(request):
     template = loader.get_template('logowanie.html')
     return HttpResponse(template.render())
+
+def guest(request):
+    template = loader.get_template('guest.html')
+    return HttpResponse(template.render())
+
+def bananas(request):
+    template = loader.get_template('bananas.html')
+    return HttpResponse(template.render())
+
+def api_artists(request):
+    artists = Artist.objects.all().values('name','surname','date_of_birth','date_of_death')
+    return JsonResponse(list(artists), safe=False)
+
+def api_exhibits(request):
+    exhibits = Exhibit.objects.select_related('id_artist').all().values(
+        'title','id_artist__name','id_artist__surname','type','height','width','weight'
+        )
+    return JsonResponse(list(exhibits), safe=False)
