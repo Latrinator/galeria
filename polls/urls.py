d72bc7955293e311e415b96d4170112b424b786f
Latrinator <ppoduszkajas53@gmail.com>
2025-01-30 13:30:36 +0100

    searching for employees

--- polls/urls.py
+++ polls/urls.py
@@ -7,7 +7,13 @@ urlpatterns = [
     path("guest/", views.guest, name='guest'),
     path("bananas/", views.bananas, name='bananas'),
     path('employee/', views.employee, name='employee'),
+    path('search/', views.search, name='search'),
     path("api/artists/", views.api_artists, name='api_artists'),
-    path("api/exhibits/", views.api_exhibits, name='api_exhibits'),
+    path('api/exhibits/', views.api_exhibits, name='api_exhibits'),
+    path('api/storages/', views.api_storages, name='api_storages'),
+    path('api/galleries/', views.api_galleries, name='api_galleries'),
+    path('api/renters/', views.api_renters, name='api_renters'),
+    path('api/rented/', views.api_rented, name='api_rented'),
+    path('api/exhibitions/', views.api_exhibitions, name='api_exhibitions')
 ]
 
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.http import JsonResponse
-from .models import Artist,Exhibit
+from .models import Artist,Exhibit,Storage,Gallery,Renter,Exhibition,Rented
 
 
 
@@ -23,12 +23,36 @@ def employee(request):
     template = loader.get_template('employee.html')
     return HttpResponse(template.render())
 
+def search(request):
+    template = loader.get_template('search.html')
+    return HttpResponse(template.render())
+
 def api_artists(request):
-    artists = Artist.objects.all().values('name','surname','date_of_birth','date_of_death')
+    artists = Artist.objects.all().values('id_artist','name','surname','date_of_birth','date_of_death')
     return JsonResponse(list(artists), safe=False)
 
 def api_exhibits(request):
     exhibits = Exhibit.objects.select_related('id_artist').all().values(
-        'title','id_artist__name','id_artist__surname','type','height','width','weight'
-        )
+        'id_exhibit','id_artist','id_artist__name','id_artist__surname','title', 'type', 'height', 'width', 'weight','status','is_valuable'
+    )
     return JsonResponse(list(exhibits), safe=False)
+
+def api_storages(request):
+    storages = Storage.objects.all().values('id_storage_session','id_exhibit', 'cause', 'since', 'until')
+    return JsonResponse(list(storages), safe=False)
+
+def api_galleries(request):
+    galleries = Gallery.objects.all().values('id_gallery','name', 'localisation')
+    return JsonResponse(list(galleries), safe=False)
+
+def api_renters(request):
+    renters = Renter.objects.all().values('id_renter','name', 'localisation')
+    return JsonResponse(list(renters), safe=False)
+
+def api_rented(request):
+    renters = Rented.objects.all().values('id_rented','id_exhibit', 'id_renter', 'since', 'until')
+    return JsonResponse(list(renters), safe=False)
+
+def api_exhibitions(request):
+    exhibitions = Exhibition.objects.all().values('id_exhibition','id_exhibit','id_gallery','since','until')
+    return JsonResponse(list(exhibitions), safe=False)
